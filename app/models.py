bd7fbb4f9d38ccc5cfedc9babed60fdfdae97cae
Edmund Mills <edmund.martin.mills@gmail.com>
2023-10-03 10:56:02 -0500

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Memo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    tags = models.ManyToManyField('Tag', blank=True)
+
+
+class Inkling(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    memo = models.ForeignKey(Memo, on_delete=models.SET_NULL, null=True, blank=True)
+    text = models.TextField()
+    tags = models.ManyToManyField('Tag', blank=True)
+
+
+class Tag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        unique_together = ['user', 'name']
+
+
+class LinkType(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ['user', 'name']
+
+
+class Link(models.Model):
+    source_inkling = models.ForeignKey('Inkling', on_delete=models.CASCADE, related_name='source_links')
+    target_inkling = models.ForeignKey('Inkling', on_delete=models.CASCADE, related_name='target_links')
+    link_type = models.ForeignKey(LinkType, on_delete=models.CASCADE)
