55a7fa0044820e586c6a98e2f86210dce7922104
Edmund Mills <edmund.martin.mills@gmail.com>
2023-10-05 12:34:11 -0500

    Add login and logout buttons

--- inklings_prototype/urls.py
+++ inklings_prototype/urls.py
@@ -23,7 +23,7 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/signup/', views.signup_view, name='signup'),
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html', next_page='home'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.MemoListView.as_view(), name='home'),
     path('memo/<int:pk>/', views.MemoDetailView.as_view(), name='view_memo'),
