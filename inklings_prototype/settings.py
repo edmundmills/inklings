864c298970d0870146118984c3feb2eb8c327c02
Edmund Mills <edmund.martin.mills@gmail.com>
2023-12-19 14:24:38 -0600

    Update staticfiles config

--- inklings_prototype/settings.py
+++ inklings_prototype/settings.py
@@ -31,8 +31,9 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
-# Application definition
-STATIC_ROOT = 'app/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
 INSTALLED_APPS = [
     'app',
     'martor',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +140,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
